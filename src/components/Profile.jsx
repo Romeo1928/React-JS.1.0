903197b0b5a4fb3d6e4ba3febe6c23a4bddb4cad
romeo <rjameson1928@gmail.com>
2022-12-07 00:31:27 +0300

    Profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img
+					src="https://media.istockphoto.com/id/1093490210/photo/aerial-view-of-nemiga-minsk-belarus.jpg?s=612x612&w=0&k=20&c=nPm03tiNqOixfWt34VpZJVauFTAZ2O30H3KaWwdj4W4="/>
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+				<div>
+					<div>post 1</div>
+					<div>post 2</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
