31b58d66b982eaa5b76b67bcca883ccff13a305e
romeo <rjameson1928@gmail.com>
2022-12-07 00:30:51 +0300

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+	return (
+		<header className = 'header'>
+			<img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg"/>
+		</header>
+	);
+}
+
+export default Header;
