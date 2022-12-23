92b5335dcf011cd54ce84871f16f80f4bdecef6a
romeo <rjameson1928@gmail.com>
2022-12-24 01:21:33 +0300

    lesson 31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,31 @@
 import s from './MyPosts.module.css';
-import React from "react";
 import Post from "./Post/Post";
+import React from "react";
 
 const MyPosts = (props) => {
 
 	let postsElements = props.posts.map((p) => {
 		return <Post message={p.message} likesCount={p.likesCount} id={p.id}/>
 	})
+	// let postsElements =
+	// 	 props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} id={p.id}/>);
+
+	let newPostElement = React.createRef();
+
+	let addPost = () => {
+		let text = newPostElement.current.value;
+		alert(text);
+	}
 
 	return (
 		<div className={s.postsBlock}>
 			<h3>My posts</h3>
 			<div>
 				<div>
-					<textarea></textarea>
+					<textarea ref={newPostElement}></textarea>
 				</div>
 				<div>
-					<button>Add post</button>
+					<button onClick={addPost}>Add post</button>
 				</div>
 			</div>
 			<div className={s.posts}>
